4036f71567d3ca28d0ba4743a9d31f60d98dfa75
Trevor Watson <watsontrevor10@gmail.com>
2020-01-28 17:05:05 -0700

    pagination added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { Heading, Header, Box, Grid, } from 'grommet'
+import { Heading, Header, Box, Grid, Select, Text } from 'grommet'
 import Person from './Person'
 
 
 const Home = () => {
   const [people, setPeople] = useState([])
+  const [page, setPage] = useState({ value: 1 })
+  const pageOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
   useEffect(() => {
-    axios.get('https://swapi.co/api/people')
+    axios.get(`https://swapi.co/api/people/?page=${page.value}`)
       .then(res => {
         setPeople(res.data.results)
       })
-  }, [])
-
+  }, [page])
 
 
   return (
@@ -21,6 +22,16 @@ const Home = () => {
       <Header alignSelf='center'>
         <Heading>Star Wars</Heading>
       </Header>
+      <div>
+        <Text>Page: </Text>
+        <Select
+          options={pageOptions}
+          value={page}
+          onChange={event => setPage({
+            value: event.value
+          })}
+        />
+      </div>
       <Box
         pad='medium'
         gap='medium'
@@ -32,12 +43,12 @@ const Home = () => {
             rows={['medium', 'flex']}
             columns={['xsmall', 'small']}
             areas={[
-              { name: 'main', start: [0,0], end: [1,0] }
+              { name: 'main', start: [0, 0], end: [1, 0] }
             ]}
           >
-            <Box 
-              gridArea='main' 
-              pad='small' 
+            <Box
+              gridArea='main'
+              pad='small'
             >
               <div key={person.url}>
                 <h1>{person.name}</h1>
